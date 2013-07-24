2bdc4556a22dd0036f55b900a9451c40b7aed626
Dustin Zhang <vollov.ca@gmail.com>
2013-07-23 23:19:45 -0400

    add login logout resource

--- src/front_end/app/js/services.js
+++ src/front_end/app/js/services.js
@@ -1,17 +1,12 @@
 'use strict';
 
-demoApp.factory('AuthenticationService', function($location) {
-  // these routes map to stubbed API endpoints in config/server.js
+demoApp.factory('AuthenticationService', function($http, $location) {
   return {
     login: function(credentials) {
-	  if(credentials.username === "dustin") {
-		  $location.path('/home');
-	  }
-      //return $http.post('/login', credentials);
+      return $http.post('/auth/login', credentials);
     },
     logout: function() {
-    	$location.path('/login');
-      //return $http.post('/logout');
+      return $http.get('/auth/logout');
     }
   };
 });
